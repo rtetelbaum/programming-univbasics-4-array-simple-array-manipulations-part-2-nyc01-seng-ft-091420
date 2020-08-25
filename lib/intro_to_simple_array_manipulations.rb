a42137fa9e92806c63f32b4fbf49aeda42b0236c
Roman Tetelbaum <rtetelbaum@mailbox.org>
2020-08-25 03:06:30 +0000

    Done.

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,23 +1,23 @@
-def using_concat(my_favorite_things, more_favs)
-  my_favorite_things.concat(more_favs)
+def using_concat(array1, array2)
+  array1.concat(array2)
 end
 
-def using_insert(list_of_programming_languages, another_language)
-  list_of_programming_languages.insert(4, another_language)
+def using_insert(array, element)
+  array.insert(4, element)
 end
 
-def using_uniq(haircuts)
-  haircuts.uniq
+def using_uniq(array)
+  array.uniq
 end
 
-def using_flatten(instruments)
-  instruments.flatten
+def using_flatten(array)
+  array.flatten
 end
 
-def using_delete(instructors, name)
-  instructors.delete(name)
+def using_delete(array, string)
+  array.delete(string)
 end
 
-def using_delete_at(famous_robots, index)
-  famous_robots.delete_at(2)
+def using_delete_at(array, index)
+  array.delete_at(index)
 end
