b16cc10b920db5668165f8eabcc7c9d8898ec981
Roman Tetelbaum <rtetelbaum@mailbox.org>
2020-08-25 03:01:34 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,23 @@
+def using_concat(my_favorite_things, more_favs)
+  my_favorite_things.concat(more_favs)
+end
+
+def using_insert(list_of_programming_languages, another_language)
+  list_of_programming_languages.insert(4, another_language)
+end
+
+def using_uniq(haircuts)
+  haircuts.uniq
+end
+
+def using_flatten(instruments)
+  instruments.flatten
+end
+
+def using_delete(instructors, name)
+  instructors.delete(name)
+end
+
+def using_delete_at(famous_robots, index)
+  famous_robots.delete_at(2)
+end
